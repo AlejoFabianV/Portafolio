13c8fb3953a73793eb6f704616a092a8dcef3ad0
Alejo Verhagen <verhagenalejo@gmail.com>
2023-05-19 19:52:54 -0300

    formato y estilo de la seccion proyectos

---
+++ src/Pages/Proyectos.js
@@ -0,0 +1,35 @@
+import React from "react";
+import '../Styles/StylePages/Proyectos.css'
+
+const Proyectos = () => {
+    return (
+        <section>
+            <div className="cartaProyecto">
+                <h1>Proyecto 1</h1>
+                <h3>Tecnologias utilizadas: </h3>
+                <p>descripcion del Proyecto</p>
+                <p>Repositorio en GitHub: </p>
+            </div>   
+            <div className="cartaProyecto">
+                <h1>Proyecto 2</h1>
+                <h3>Tecnologias utilizadas: </h3>
+                <p>descripcion del Proyecto</p>
+                <p>Repositorio en GitHub: </p>
+            </div>  
+            <div className="cartaProyecto">
+                <h1>Proyecto 3</h1>
+                <h3>Tecnologias utilizadas: </h3>
+                <p>descripcion del Proyecto</p>
+                <p>Repositorio en GitHub: </p>
+            </div>  
+            <div className="cartaProyecto">
+                <h1>Proyecto 4</h1>
+                <h3>Tecnologias utilizadas: </h3>
+                <p>descripcion del Proyecto</p>
+                <p>Repositorio en GitHub: </p>
+            </div>   
+        </section>
+    );
+}
+
+export default Proyectos;
