660857f90c2e0021291908a03ffe521f5e6b13e5
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-02 21:03:06 -0300

    seccion foot y su estilo

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -4,7 +4,12 @@ import '../../Styles/StyleComponents/StylesLayout/Footer.css'
 const Footer = () => {
     return (
         <footer>
-            <p>Este es el footer</p>
+            <p>Diseño hecho por Alejo 2023</p>
+            <div>
+                <p><a href="https://github.com/AlejoFabianV" target="BLANK"><i className="fa-brands fa-github fa-lg" /> AlejoFVerhagen</a></p>
+                <p><a href="https://www.linkedin.com/in/alejo-fabian-verhagen-2a149822b/" target="BLANK"><i className="fa-brands fa-linkedin" /> Alejo Vehragen</a></p>
+                <p><a href="https://mail.google.com/mail/u/0/#inbox" target="BLANK"><i className="fa-solid fa-envelope" /> verhagenalejo@gmail.com</a></p>
+            </div>
         </footer>
     );
 }
