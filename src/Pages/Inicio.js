d8ab2b0f2d905f4027a33d6b61901f66efbcf101
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-05 23:14:01 -0300

    creada la seccion de inicio

--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -1,10 +1,17 @@
 import React from "react";
 import '../Styles/StylePages/Inicio.css'
+import luffyImage from '../Img/luffyInicio.png'
 
 const Inicio = () => {
     return (
-        <section>
-            <p>esto es el Inicio</p>
+        <section className="backGroundInicio">
+            <div className="containerInicio">
+                <h1 className="titleWelcome">Alejo Verhagen</h1>
+                <h1 className="titleWelcome">Developer Full Stack</h1>
+            </div>
+            <div className="backGroundImg">
+                <img src={luffyImage} alt="..."/>
+            </div>
         </section>
     );
 }
