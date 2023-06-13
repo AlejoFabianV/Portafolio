86e18520e2b94e37df8e15bb9fa4a1274750a300
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-13 18:44:17 -0300

    agregue las dos secciones de sobre mi

--- src/Pages/SobreMI.js
+++ src/Pages/SobreMI.js
@@ -4,8 +4,19 @@ import '../Styles/StylePages/SobreMi.css'
 const SobreMi = () => {
     return (
         <section className="backGroundSobreMi">
-            <article>este es mi presentacion</article>
-            <article>las tecnologias que se</article>
+            <div className="presentation">
+                <h3>Hi</h3>
+                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+                </p>
+            </div>
+            <div className="tecnologies">
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                </ul>
+            </div>
         </section>
     );
 }
