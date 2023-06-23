8c8817236864bafb978611f8cc20f9e3dc32b547
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-23 15:53:30 -0300

    config del ruteo

--- src/Components/Layout/Nav.js
+++ src/Components/Layout/Nav.js
@@ -8,8 +8,8 @@ const Nav = () => {
         <nav>
             <ul>
                 <li><NavLink to="/"><b>Inicio</b></NavLink></li>
-                <li><NavLink to="SobreMi"><b>Sobre Mi</b></NavLink></li>
-                <li><NavLink to="Proyectos"><b>Proyectos</b></NavLink></li>
+                <li><NavLink to="/SobreMi"><b>Sobre Mi</b></NavLink></li>
+                <li><NavLink to="/Proyectos"><b>Proyectos</b></NavLink></li>
             </ul>
         </nav>
     );
