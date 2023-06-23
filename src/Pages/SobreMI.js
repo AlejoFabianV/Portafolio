e24517e02cd6d17b3ef2f336a0f46d803e0bda44
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-23 16:14:58 -0300

    nombre del img arreglado

--- src/Pages/SobreMI.js
+++ src/Pages/SobreMI.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../Styles/StylePages/SobreMi.css'
-import SQLServerimg from '../Img/SQLserver.png'
+import sqlServerImg from '../Img/SQLserver.png'
 import dotnet from '../Img/dotnet.png'
 
 const SobreMi = () => {
@@ -27,7 +27,7 @@ const SobreMi = () => {
                         <h4>MySQL</h4>
                     </div>
                     <div>
-                        <img src={SQLServerimg} alt="ImgTecnologie" />
+                        <img src={sqlServerImg} alt="ImgTecnologie" />
                         <h4>SQL Server</h4>
                     </div>
                     <div>
