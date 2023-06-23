dccb512b0fff1c624377c2295b4bb53bcea117f6
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-23 15:23:32 -0300

    presentcaion para proyectos

--- src/Pages/Proyectos.js
+++ src/Pages/Proyectos.js
@@ -1,45 +1,70 @@
 import React from "react";
 import '../Styles/StylePages/Proyectos.css'
+import previewProyecto1 from '../Img/previewProyecto/Los-diez-anillos-preview.png'
 
 const Proyectos = () => {
     return (
         <section className="backGroundProyectos">
             <div className="cartaProyecto">
-                <h1>Proyecto 1</h1>
-                <h3>Tecnologias utilizadas: </h3>
-                <p>descripcion del Proyecto</p>
-                <p>Repositorio en GitHub: </p>
-            </div>   
-            <div className="cartaProyecto">
-                <h1>Proyecto 2</h1>
-                <h3>Tecnologias utilizadas: </h3>
-                <p>descripcion del Proyecto</p>
-                <p>Repositorio en GitHub: </p>
+                <h1>Tienda digital de comics/mangas</h1>
+                <h3>React.js, Node,js, Express, CSS </h3>
+                <p>
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/back-10-anillos" target="blank"> Backend</a>
+                    <br />
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/front-10-anillos" target="blank"> Frontend</a>
+                </p>
+                <a href="https://10-anillos.vercel.app" target="blank"><img href='https://10-anillos.vercel.app' src={previewProyecto1} alt="preview" /></a>
             </div>  
             <div className="cartaProyecto">
-                <h1>Proyecto 3</h1>
-                <h3>Tecnologias utilizadas: </h3>
-                <p>descripcion del Proyecto</p>
-                <p>Repositorio en GitHub: </p>
+                <h1>Tienda digital de comics/mangas</h1>
+                <h3>React.js, Node,js, Express, CSS </h3>
+                <p>
+                    <i class="fa fa-github" aria-hidden="true" /> <a href="https://github.com/AlejoFabianV/back-10-anillos" target="blank"> Backend</a>
+                    <br />
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/front-10-anillos" target="blank"> Frontend</a>
+                </p>
+                <a href="https://10-anillos.vercel.app" target="blank"><img href='https://10-anillos.vercel.app' src={previewProyecto1} alt="preview" /></a>
+            </div> 
+            <div className="cartaProyecto">
+                <h1>Tienda digital de comics/mangas</h1>
+                <h3>React.js, Node,js, Express, CSS </h3>
+                <p>
+                    <i class="fa fa-github" aria-hidden="true" /> <a href="https://github.com/AlejoFabianV/back-10-anillos" target="blank"> Backend</a>
+                    <br />
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/front-10-anillos" target="blank"> Frontend</a>
+                </p>
+                <a href="https://10-anillos.vercel.app" target="blank"><img href='https://10-anillos.vercel.app' src={previewProyecto1} alt="preview" /></a>
+            </div> 
+            <div className="cartaProyecto">
+                <h1>Tienda digital de comics/mangas</h1>
+                <h3>React.js, Node,js, Express, CSS </h3>
+                <p>
+                    <i class="fa fa-github" aria-hidden="true" /> <a href="https://github.com/AlejoFabianV/back-10-anillos" target="blank"> Backend</a>
+                    <br />
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/front-10-anillos" target="blank"> Frontend</a>
+                </p>
+                <a href="https://10-anillos.vercel.app" target="blank"><img href='https://10-anillos.vercel.app' src={previewProyecto1} alt="preview" /></a>
             </div>  
             <div className="cartaProyecto">
-                <h1>Proyecto 4</h1>
-                <h3>Tecnologias utilizadas: </h3>
-                <p>descripcion del Proyecto</p>
-                <p>Repositorio en GitHub: </p>
-            </div>   
+                <h1>Tienda digital de comics/mangas</h1>
+                <h3>React.js, Node,js, Express, CSS </h3>
+                <p>
+                    <i class="fa fa-github" aria-hidden="true" /> <a href="https://github.com/AlejoFabianV/back-10-anillos" target="blank"> Backend</a>
+                    <br />
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/front-10-anillos" target="blank"> Frontend</a>
+                </p>
+                <a href="https://10-anillos.vercel.app" target="blank"><img href='https://10-anillos.vercel.app' src={previewProyecto1} alt="preview" /></a>
+            </div> 
             <div className="cartaProyecto">
-                <h1>Proyecto 5</h1>
-                <h3>Tecnologias utilizadas: </h3>
-                <p>descripcion del Proyecto</p>
-                <p>Repositorio en GitHub: </p>
-            </div>  
-            <div className="cartaProyecto">
-                <h1>Proyecto 6</h1>
-                <h3>Tecnologias utilizadas: </h3>
-                <p>descripcion del Proyecto</p>
-                <p>Repositorio en GitHub: </p>
-            </div>  
+                <h1>Tienda digital de comics/mangas</h1>
+                <h3>React.js, Node,js, Express, CSS </h3>
+                <p>
+                    <i class="fa fa-github" aria-hidden="true" /> <a href="https://github.com/AlejoFabianV/back-10-anillos" target="blank"> Backend</a>
+                    <br />
+                    <i class="fa fa-github" aria-hidden="true" /><a href="https://github.com/AlejoFabianV/front-10-anillos" target="blank"> Frontend</a>
+                </p>
+                <a href="https://10-anillos.vercel.app" target="blank"><img href='https://10-anillos.vercel.app' src={previewProyecto1} alt="preview" /></a>
+            </div> 
         </section>
     );
 }
