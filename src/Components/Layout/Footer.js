96974e4803b77c090a7ecf282a1aab4de6c6d63e
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-24 15:48:58 -0300

    saque el href del mail

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <div>
                 <p><b><a href="https://github.com/AlejoFabianV" target="BLANK"><i className="fa-brands fa-github fa-lg" /> AlejoFVerhagen</a></b></p>
                 <p><b><a href="https://www.linkedin.com/in/alejo-fabian-verhagen-2a149822b/" target="BLANK"><i className="fa-brands fa-linkedin" /> Alejo Vehragen</a></b></p>
-                <p><b><a href="https://mail.google.com/mail/u/0/#inbox" target="BLANK"><i className="fa-solid fa-envelope" /> verhagenalejo@gmail.com</a></b></p>
+                <p><b><i className="fa-solid fa-envelope" /> verhagenalejo@gmail.com</b></p>
             </div>
         </footer>
     );
