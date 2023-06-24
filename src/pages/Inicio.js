6d60030a0c052e99621a353896f208e21554e771
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-24 16:29:36 -0300

    agegue href para descar de cv y lo estile

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -7,10 +7,8 @@ const Inicio = () => {
             <div className="containerInicio">
                 <h1 className="titleWelcome">Alejo Verhagen</h1>
                 <h1 className="titleWelcome">Developer Full Stack</h1>
+                <a className="downloadCV" href='./Verhagen-Alejo-CV.pdf' download>Descargar CV</a>
             </div>
-            {/* <div className="backGroundImg">
-                <img src={luffyImage} alt="..."/>
-            </div> */}
         </section>
     );
 }
