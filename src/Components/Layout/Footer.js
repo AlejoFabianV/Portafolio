8242e2f64c9db96794e8bca262999d99bc6aa3c0
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-27 12:45:25 -0300

    elimine el texto del footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -4,7 +4,6 @@ import '../../styles/styleComponents/stylesLayout/Footer.css'
 const Footer = () => {
     return (
         <footer>
-            <p><b>Diseño hecho por Alejo 2023</b></p>
             <div>
                 <p><b><a href="https://github.com/AlejoFabianV" target="BLANK"><i className="fa-brands fa-github fa-lg" /> AlejoFVerhagen</a></b></p>
                 <p><b><a href="https://www.linkedin.com/in/alejo-fabian-verhagen-2a149822b/" target="BLANK"><i className="fa-brands fa-linkedin" /> Alejo Vehragen</a></b></p>
